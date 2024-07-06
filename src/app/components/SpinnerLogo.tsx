e32b061b016633ff931e00cd90eaf716912992e0
vernellm <vtmangum@gmail.com>
2024-07-06 18:00:22 -0500

    added landing page and routes

---
+++ src/app/components/SpinnerLogo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BackgroundVideo from 'next-video/background-video';
+import spinnerLogo from '../../../videos/0001-0250.mp4'
+import Link from 'next/link'
+
+export default function SpinnerLogo() {
+  return (
+    <div className='w-full items-center'>
+        <Link href="/home" className='cursor-default'>
+          <BackgroundVideo className='h-screen w-screen' src={spinnerLogo}/>
+        </Link>
+      </div>
+  )
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,30 @@
+import BackgroundVideo from 'next-video/background-video';
+import React from 'react'
+import spinnerLogo from '../../../videos/0001-0250.mp4'
+
+function Home() {
+  return (
+    <main className='bg-neutral-950 mx-auto min-h-screen'>
+      <div className='w-full items-center'>
+        <BackgroundVideo className='h-screen w-screen' src={spinnerLogo}>
+          <label className="input input-ghost flex items-center gap-2 mx-auto">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70">
+              <path
+                fillRule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clipRule="evenodd"/>
+            </svg>
+            <input type="password" className="grow" value="password" />
+          </label>
+        </BackgroundVideo>
+      </div>
+
+    </main>
+  )
+}
+
+export default Home
